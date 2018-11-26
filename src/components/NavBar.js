33fd5fbac5d55f4af3c01f43e041aed23f65c25b
SkipCat <cecilehuguet@laposte.net>
2018-11-26 12:55:41 +0100

    Added routing

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends Component {
+
+  render() {
+    return (
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    );
+  }
+}
