29a799b96955103a187e9c35270d66f9c3d0cb6e
SkipCat <cecilehuguet@laposte.net>
2018-12-10 11:36:58 +0100

    Added heroku database settings

---
+++ build/precache-manifest.bccb5dbad198baec8ff88bcf14c22865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa800da4ee0e959e4165",
+    "url": "/static/js/main.fa800da4.chunk.js"
+  },
+  {
+    "revision": "49967d28c1bd128447e1",
+    "url": "/static/js/1.49967d28.chunk.js"
+  },
+  {
+    "revision": "fa800da4ee0e959e4165",
+    "url": "/static/css/main.88c1fe98.chunk.css"
+  },
+  {
+    "revision": "4f74590e1c0dd4b0606cb9b36500b7bf",
+    "url": "/index.html"
+  }
+];
